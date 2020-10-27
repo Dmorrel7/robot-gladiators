0409eba5077d879f9e4fd6f5df6b23c679fe3600
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-10-27 10:42:30 -0400

    first round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,30 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of playerAttack from enemyHealth
+    enemyHealth = enemyHealth - playerAttack;
+    //Log a resulting message to the console so we know it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+    //Subtract the value enemyAttack from playerHealth
+    playerHealth = playerHealth - enemyAttack;
+    //Log a ressulting mewssage to console so we know it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
